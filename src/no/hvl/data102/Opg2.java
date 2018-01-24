dcb7d774ddbfdc8abb935e3acb5d12893c5ea1e8
David <david.hyldgaard@gmail.com>
2018-01-24 15:51:45 +0100

    s

---
+++ src/no/hvl/data102/Opg2.java
@@ -0,0 +1,57 @@
+package no.hvl.data102;
+
+public class Opg2 {
+/*
+ * 2a)
+ * 
+ * i) O(n^2)
+ * ii) O(n)
+ * iii) O(n^3)
+ * iv) O(log2 n)
+ * 
+ *  2b)
+ *  
+ *  antal tilordninger er t(n)=n^2 da antal gennemgange af løkken vokser med n^2. Dermed får vi også O(n^2)
+ *  
+ *  2c)
+ *  
+ *  Algoritme A:
+ *  Antal tildelinger: n
+ *  Addisjoner: n
+ *  multiplikasjoner: 0
+ *  Divsjoner: 0
+ *  Total antal operasjoner: n
+ *  
+ *  Algoritme B:
+ *  
+ *  Antal tildelinger: n^2
+ *  Addisjoner: n^2
+ *  multiplikasjoner: 0
+ *  Divsjoner: 0
+ *  Total antal operasjoner: n
+ *  
+ *  Algoritme C:
+ *  
+ *  Antal tildelinger: k
+ *  Addisjoner: 1
+ *  multiplikasjoner: 1
+ *  Divsjoner: 1
+ *  Total antal operasjoner: k
+ *  
+ *  2d)
+ *  log n = 0.00002 sekunder (Så vidt jeg kan se...)
+ *  n = 1 sekund
+ *  n*log n = 19.93 sekunder.
+ *  n^2 = 11.57 dage
+ *  n^3 = 31709.8år (I know...) // (((10^6)^3)/10^6)/(60*60*24*365)
+ *  
+ *  2e)
+ *  t(n) = (n-2)(n-1) = n^2 - 3n + 2
+ *  O(n^2)
+ *  
+ *  
+ *  
+ *  
+ * 
+ */
+}
