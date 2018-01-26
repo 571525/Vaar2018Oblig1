1a9449a8a68bcffac6ed2f2e88535a353189cb7d
Frederik <frederikjvs@gmail.com>
2018-01-26 18:08:20 +0100

    Fungerer ikke enda, ser på det videre senere

--- src/no/hvl/data102/CDarkiv2.java
+++ src/no/hvl/data102/CDarkiv2.java
@@ -1,7 +1,70 @@
 package no.hvl.data102;
 
+import no.hvl.data102.CD.Genre;
+import no.hvl.data102.adt.CDarkivADT;
+
 public class CDarkiv2 implements CDarkivADT {
 	
+	private int antall;
+	private LinearNode<CD> start;
+	
+	public CDarkiv2() {
+		start = null;
+		antall = 0;
+	}
+	
+	
+	@Override
+	public CD[] hentCdTabell() {
+		return LinearNode<CD>; //Hvordan skal dette gjøres ??? dette fungerer jo ikke eheh
+	}
 
+	@Override
+	public void leggTilCd(CD nyCd) {
+		LinearNode<CD> newNode = new LinearNode<CD>(nyCd);
+		newNode.setNext(start); // Setter node til start 
+		start = newNode; //Nå er start den nye noden
+		antall++;
+		
+	}
+
+	@Override
+	public boolean slettCd(int cdNr) {
+		boolean delete = false;
+		if(antall>0) {
+			
+			delete = start.getElement();
+			
+			antall--;
+			delete = true;
+		}
+		return delete;
+	}
+
+	@Override
+	public CD[] sokTittel(String delstreng) {
+		
+		return null;
+	}
+
+	@Override
+	public CD[] sokArtist(String delstreng) {
+		
+		return null;
+	}
+
+	@Override
+	public int hentAntall() {
+		
+		return 0;
+	}
+
+	@Override
+	public int hentAntall(Genre sjanger) {
+		
+		return 0;
+	}
+		
+	
 	
 }
